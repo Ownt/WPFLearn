95b2b5e5c36d60490db962df04d6e4d0cfb31e62
Никита Лобов <nikitanonfem@gmail.com>
2023-02-23 14:59:25 +0300

    Базовый класс модели-представления

---
+++ WPFLearn/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFLearn.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
