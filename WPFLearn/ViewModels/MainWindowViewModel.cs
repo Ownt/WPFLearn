06043fad8720a5d0ca0fd47642d057617e4111f4
Никита Лобов <nikitanonfem@gmail.com>
2023-02-23 19:42:24 +0300

    Создание команды закрытия приложения

--- WPFLearn/ViewModels/MainWindowViewModel.cs
+++ WPFLearn/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using WPFLearn.ViewModels.Base;
+﻿using System.Windows;
+using System.Windows.Input;
+using WPFLearn.Infrastructure.Commands;
+using WPFLearn.ViewModels.Base;
 
 namespace WPFLearn.ViewModels
 {
@@ -6,10 +9,36 @@ namespace WPFLearn.ViewModels
     {
 
         #region Заголовок главного окна
+        
         private string _Title = "Главное окно";
         /// <summary> Заголовок окна</summary>
-        public string Title { get => _Title; set => Set(ref _Title, value); } 
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
         #endregion
 
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        } 
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel() 
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+
+            #endregion
+        }
     }
 }
