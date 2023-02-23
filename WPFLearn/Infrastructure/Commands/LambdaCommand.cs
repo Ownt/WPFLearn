e61d5fe7fdda79606250690d28d4831049af0c33
Никита Лобов <nikitanonfem@gmail.com>
2023-02-23 17:51:36 +0300

    Главный класс команды для приложения

---
+++ WPFLearn/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFLearn.Infrastructure.Commands.Base;
+
+namespace WPFLearn.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool>? canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool>? CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter)
+            => canExecute?.Invoke(arg: parameter)?? true;
+
+        public override void Execute(object? parameter) => execute(obj: parameter);
+    }
+}
