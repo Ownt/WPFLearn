d39a893c43b340428d89b047b8fdf93280c2f40a
Никита Лобов <nikitanonfem@gmail.com>
2023-02-25 14:16:28 +0300

    Добавление новых полей в класс Service

--- WPFLearn/Models/Service.cs
+++ WPFLearn/Models/Service.cs
@@ -2,6 +2,13 @@
 {
     internal class Service
     {
+        /// <summary> Название услуги </summary>
         public string? Name { get; set; }
+
+        /// <summary> Цена услуги</summary>
+        public decimal? Price { get; set; } 
+
+        /// <summary> Описание услуги </summary>
+        public string? Description { get; set; }   
     }
 }
