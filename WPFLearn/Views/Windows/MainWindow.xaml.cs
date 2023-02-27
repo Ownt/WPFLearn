842a4d31bb567643ad36e1b8c08a6153dc9824cb
Никита Лобов <nikitanonfem@gmail.com>
2023-02-27 20:16:12 +0300

    Реализация фельтрации договоров в разметке кода файла обработчика событий окна

--- WPFLearn/Views/Windows/MainWindow.xaml.cs
+++ WPFLearn/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WPFLearn.Models;
 
 namespace WPFLearn
 {
@@ -10,6 +14,26 @@ namespace WPFLearn
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ContractsCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Contracts contract)) return;
+            if (contract.Name is null) return;
+            
+            var filter_text = ContractNameFilterText.Text;
+            if (filter_text.Length == 0) return; 
+
+            if (contract.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnContractFilterTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var text_box = (TextBox) sender;
+            var collection = (CollectionViewSource)text_box.FindResource("ContractsCollection");
+            collection.View.Refresh();
         }
     }
 }
